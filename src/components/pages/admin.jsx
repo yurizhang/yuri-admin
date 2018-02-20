9412bbab94c7925dc1b060c95cc9e13aec172c10
Yuri Zhang <13913903724@126.com>
2018-02-20 23:45:05 +0800

    first commit

---
+++ src/components/pages/admin.jsx
@@ -0,0 +1,205 @@
+import React from "react"
+//import ReactDOM from "react-dom"
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Menu, Breadcrumb, Icon, Dropdown, Row, Col } from 'antd';
+import './admim.css';
+import { menus as menusList } from '../../constants/menuslist';  // 菜单数据 
+import Crouter from "../../routes/index";  //路由数据
+
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  }
+  onTrigg = () => {
+    
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+ menu = ()=>{return(
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+    </Menu.Item>
+  </Menu>
+)};
+
+  render() {
+    let menu=this.menu();
+    const routes = [
+  {
+    path: "/admin",
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/admin/bubblegum",
+    sidebar: () => <div>bubblegum!</div>,
+    main: () => <h2>Bubblegum</h2>
+  },
+  {
+    path: "/admin/shoelaces",
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => {return (<h2>Shoelaces2222</h2>)}
+  }
+];
+
+const renderMenuItem =
+    ({ key, title, icon, link, ...props }) =>
+        <Menu.Item
+            key={key || link}
+            {...props}
+        >
+            <Link to={link || key}>
+                {icon && <Icon type={icon} />}
+                <span className="nav-text">{title}</span>
+            </Link>
+        </Menu.Item>;
+
+const renderSubMenu =
+    ({ key, title, icon, link, sub, ...props }) =>
+        <Menu.SubMenu
+            key={key || link}
+            title={
+                <span>
+                    {icon && <Icon type={icon} />}
+                    <span className="nav-text">{title}</span>
+                </span>
+            }
+            {...props}
+        >
+            {sub && sub.map(item => renderMenuItem(item))}
+        </Menu.SubMenu>;
+
+
+    return (
+        <Router>
+      <Layout style={{ minHeight: '100vh' }} id="components-layout-demo-custom-trigger">
+        <Sider           
+          breakpoint="lg"          
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+
+          {menusList && menusList.map(item => item.sub && item.sub.length ?
+            renderSubMenu(item) : renderMenuItem(item)
+          )}
+
+            {/* <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <Link to="/admin/bubblegum"><span className="nav-text">Option 1</span></Link>
+            </Menu.Item>
+
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <Link to="/admin/shoelaces"><span className="nav-text">Option 2</span></Link>
+            </Menu.Item>
+
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span><Link to="/admin">首页</Link></span>
+            </Menu.Item> */}
+
+
+          </Menu>
+        </Sider>
+
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Row type="flex" justify="space-between" align="middle">
+                <Col span={22}>
+                <Icon className="trigger" type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.onTrigg} />
+                </Col>
+                <Col span={2}>
+                    <Dropdown overlay={menu}>
+                        <a className="ant-dropdown-link" href="javascript:;">
+                        Admin <Icon type="down" />
+                        </a>
+                    </Dropdown>
+                </Col>
+            </Row>
+
+                
+                
+ 
+          
+          </Header>
+          <Content style={{ margin: '0 16px' }}>
+
+
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+
+   
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              路由渲染到这里
+              <Crouter></Crouter>
+
+             {/*
+              {routes.map((route, index) => (
+         
+                <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.main}
+                />
+              ))}
+            */}
+            </div>
+
+
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Yuri
+          </Footer>
+        </Layout>
+      </Layout>
+      </Router>
+    );
+  }
+}
+
+
+export default SiderDemo
+//ReactDOM.render(<SiderDemo />, document.getElementById("root"));
+
+// #components-layout-demo-side .logo {
+//   height: 32px;
+//   background: rgba(255,255,255,.2);
+//   margin: 16px;
+// }
---
+++ src/routes/index.js
@@ -0,0 +1,104 @@
+/**
+ * Created by Yuri Zhang  2017/8/13.
+ */
+import React, { Component } from 'react';
+// import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+import BasicForm from '../components/forms/BasicForm';
+
+
+import Icons from '../components/ui/Icons';
+import Buttons from '../components/ui/Buttons';
+//import Spins from '../components/ui/Spins';
+import Modals from '../components/ui/Modals';
+import Notifications from '../components/ui/Notifications';
+import Tabs from '../components/ui/Tabs';
+//import Banners from '../components/ui/banners';
+//import Drags from '../components/ui/Draggable';
+
+//import Gallery from '../components/ui/Gallery';
+
+
+//import AuthBasic from '../components/auth/Basic';
+//import RouterEnter from '../components/auth/RouterEnter';
+//import Wysiwyg from 'bundle-loader?lazy!../components/ui/Wysiwyg';  // 按需加载富文本配置
+import Bundle from '../components/widget/BundlePro';
+import asyncComponent from '../components/widget/BundlePro2';
+//import Cssmodule from '../components/cssmodule';
+
+
+// 按需加载Button配置
+const ButtonsBundle = (props) => (
+    <Bundle load={() => import('../components/ui/Buttons')}>
+        {(ButtonsBundle) => <ButtonsBundle {...props} />}
+    </Bundle>
+);
+
+const ButtonsBundle2 = asyncComponent(() => import("../components/ui/Buttons"));
+
+// 按路由拆分代码
+
+const MyLoadingComponent = ({ isLoading, error }) => {
+    // Handle the loading state
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    // Handle the error state
+    else if (error) {
+        return <div>Sorry, there was a problem loading the page.</div>;
+    }
+    else {
+        return null;
+    }
+};
+const ButtonsBundle3 = Loadable({
+    loader: () => import('../components/ui/Buttons'),
+    loading: MyLoadingComponent
+});
+
+const IconsBundle = Loadable({
+    loader: () => import('../components/ui/Icons'),
+    loading: MyLoadingComponent
+});
+
+export default class CRouter extends Component {
+    requireAuth = (permission, component) => {
+        const { auth } = this.props;
+        const { permissions } = auth.data;
+        // const { auth } = store.getState().httpData;
+        if (!permissions || !permissions.includes(permission)) return <Redirect to={'404'} />;
+        return component;
+    };
+    render() {
+        return (
+            
+            <Switch>
+                <Route exact path="/" component={BasicForm} />
+                <Route exact path="/app/form/basicForm" component={BasicForm} />
+                <Route exact path="/app/ui/icons" component={IconsBundle} />
+                <Route exact path="/app/ui/buttons" component={ButtonsBundle3} />
+                {/* <Route exact path="/app/ui/spins" component={Spins} /> */}
+                <Route exact path="/app/ui/modals" component={Modals} />
+                <Route exact path="/app/ui/notifications" component={Notifications} />
+                <Route exact path="/app/ui/tabs" component={Tabs} />
+                {/* <Route exact path="/app/ui/banners" component={Banners} /> */}
+              
+                {/* <Route exact path="/app/ui/drags" component={Drags} /> */}
+                {/* <Route exact path="/app/ui/gallery" component={Gallery} /> */}
+
+            
+
+                {/* <Route exact path="/app/auth/basic" component={AuthBasic} />
+                <Route exact path="/app/auth/routerEnter" component={(props) => this.requireAuth('auth/testPage', <RouterEnter {...props} />)} />
+
+                <Route exact path="/app/cssModule" component={Cssmodule} /> */}
+
+               
+
+                <Route render={() => <Redirect to="/404" />} />
+            </Switch>
+        )
+    }
+}
